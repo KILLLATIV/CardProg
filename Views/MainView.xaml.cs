06e41ce6f34f08169ca12d7b6cee5ce4c63981e7
KILLLATIV <48354935+KILLLATIV@users.noreply.github.com>
2021-03-24 23:59:43 +0300

    Add Main View & Read View interface

---
+++ Views/MainView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using CardReaderRecorder.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CardReaderRecorder.Views
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        private void Read_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new ReadViewModel();
+        }
+    }
+}
